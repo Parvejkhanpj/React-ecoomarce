0e7316c1988e10c139a978f6e69ceca2be1e530e
Parvej khan <kparvej761@gmail.com>
2022-05-09 01:08:12 +0530

    make home page

--- e-commarce/src/App.js
+++ e-commarce/src/App.js
@@ -1,19 +1,23 @@
 import "./style.css";
-import Navbar from "./Component/Navbar";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Product from "./Component/Product";
 import Login from "./Component/Login";
+import Cart from "./Component/Cart";
+import Home from "./Pages/Home";
+import Navbar from "./Component/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
-
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/product" element={<Product />} />
-          <Route path="/signin" element={<Login />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/cart" element={<Cart />} />
         </Routes>
       </BrowserRouter>
     </div>
--- e-commarce/src/Component/Navbar.js
+++ e-commarce/src/Component/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BsCartCheck, BsFillPersonFill, BsSearch } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import Cart from "./Cart";
+import Product from "./Product";
 
 export default function Navbar() {
   return (
     <div className="nav-section">
-      <img src="./images/Logo.png" width="180px" />
+      <img src="./images/Logo.png" width="180px" alt="logo" />
       {/* <div className="search-box">
         <input placeholder="Search Your Favriate" />
         <button>
@@ -21,22 +23,26 @@ export default function Navbar() {
           aria-label="Search"
         />
         <button className="btn btn-outline-success" type="submit">
-          Search
+          <BsSearch />
         </button>
       </form>
 
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" className="menu-item">
+              Home
+            </Link>
           </li>
           <li>
-            {" "}
-            <Link to="/product">Product</Link>
+            <Link to="/product" className="menu-item">
+              Product{Product}
+            </Link>
           </li>
           <li>
-            {" "}
-            <Link to="/prime">Prime</Link>
+            <Link to="/prime" className="menu-item">
+              Prime
+            </Link>
           </li>
         </ul>
       </nav>
@@ -46,13 +52,19 @@ export default function Navbar() {
         className="icon--title
       "
       >
-        <BsCartCheck /> <Link to="/Cart">Cart</Link>
+        <BsCartCheck />{" "}
+        <Link to="/Cart" className="menu-item">
+          Cart {Cart}
+        </Link>
       </p>
       <p
         className="icon--title
       "
       >
-        <BsFillPersonFill /> <Link to="/login">Login</Link>
+        <BsFillPersonFill />{" "}
+        <Link to="/login" className="menu-item">
+          Login
+        </Link>
       </p>
     </div>
   );
