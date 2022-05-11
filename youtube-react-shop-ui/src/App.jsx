8017460d7df5645a1cc7efd96b1d2334efa973fc
Parvej khan <kparvej761@gmail.com>
2022-05-12 00:23:15 +0530

    update ui

---
+++ youtube-react-shop-ui/src/App.jsx
@@ -0,0 +1,26 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:categery" element={<ProductList />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/product/dp/:id" element={<Product />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
